7a3d8697675b1751240bef88257a1f3990ae1898
Skander Jabouzi <jabouzi@gmail.com>
2015-10-14 10:43:06 +0000

    current date
    git-svn-id: svn+ssh://jabouzi.com/home/jabouzic/svn/salatCPP/trunk@14 d2854926-a808-42be-b03e-45614320a301

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+#include <ctime>
 #include "Salat.h"
 using namespace std;
 
 int main()
 {
 	string salatTimes[7] = {"","","","","","",""};;
-    int year = 2015;
-    int month = 4;
-    int day = 10;
+	time_t t = time(0);  
+	struct tm * now = localtime( & t );
+    int year = now->tm_year + 1900;
+    int month = now->tm_mon + 1;
+    int day = now->tm_mday;
+    cout << year << " - " << month << " - " << day << "\n";
     int calcMethod = 2;
     int asrMethod = 0;
     int highLatitude = 0;
